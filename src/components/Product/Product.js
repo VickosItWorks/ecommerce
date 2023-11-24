edd9297600e26a774a545e465966ead180ae7020
Vanesa Rostand <vanerostand@gmail.com>
2023-11-24 19:19:38 +0100

    product list

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { useFetch } from "../../hooks/useFetch";
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding: 0.5em;
+  margin: 0 auto;
+  align-items: center;
+`;
 
 const Product = () => {
   const { data, error } = useFetch("/products");
 
   return (
     <>
-      <h1>Product</h1>
+      <h1>Products</h1>
       {data && data.map((product) => (
-        <div key={product.id}>
+        <Card key={product.id}>
           <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
-        </div>
+          <p>Description: {product.description}</p>
+          <p>$ {product.price}</p>
+        </Card>
       ))}
     </>
   );
